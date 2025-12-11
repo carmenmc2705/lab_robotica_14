b53138123ee18fafccd8a1c686215f8e02f5b3bf
carmenmc2705 <carmenmc2705@gmail.com>
2025-12-11 12:07:07 +0100

    pract3

--- docker/docker/run_container_windows.bat
+++ docker/docker/run_container_windows.bat
@@ -1,8 +1,10 @@
+docker rm lab_rob_container
 docker run -it ^
 --env="DISPLAY=host.docker.internal:0" ^
 --name lab_rob_container ^
 --net=host ^
 --privileged ^
---mount type=bind,source=C:\Users\mapy7\lab_rob_shared,target=/home/grupo14/lab_rob_shared ^
+--mount type=bind,source=C:\Users\carme\lab_rob_shared,target=/home/grupo14/lab_rob_shared ^
 lab_rob_image ^
 bash
+
