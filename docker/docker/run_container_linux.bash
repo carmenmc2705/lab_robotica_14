119ae333bee746617b84a128b00c18270fef2473
carmenmc2705 <carmenmc2705@gmail.com>
2025-12-18 13:42:14 +0100

    cambios

--- docker/docker/run_container_linux.bash
+++ docker/docker/run_container_linux.bash
@@ -1,14 +1,11 @@
 
-xhost +local:docker
-docker run -it \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+docker run -it --rm \
+    --env="DISPLAY=host.docker.internal:0" \
     --name lab_rob_container \
     --net=host \
     --privileged \
-    --mount type=bind,source=/home/[USER]/lab_rob_shared,target=/home/[USER]/lab_rob_shared \
+    --mount type=bind,source="/mnt/c/Users/carme/lab_rob_shared",target="/home/grupo14/lab_rob_shared" \
+    --env="ROS_MASTER_URI=http://172.20.10.2:11311" \
+    --env="ROS_HOSTNAME=172.20.10.2" \
     lab_rob_image \
     bash
-    
-docker rm lab_rob_container
